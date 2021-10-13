3efa9cf02ec1c5e8e86ff13f702eb8e7ec47b76d
nathangiose <nathangiose@gmail.com>
2021-10-13 15:36:25 +0200

    Conditional Rendering

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     //data, funtions, component templates
     data() {
         return{
+            showBooks: true, //this is to show and hide the information
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
@@ -15,7 +16,12 @@ const app = Vue.createApp({
             // console.log("You clicked me")
             // this.title = 'Words of Radiance'
             this.title = title
+           
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks //this toggles it from true to false and vice
         }
+        
     }
 })
 
