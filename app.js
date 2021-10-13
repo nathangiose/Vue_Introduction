176655d14e7952f253a3abbecb7ac80c7cb2bda7
nathangiose <nathangiose@gmail.com>
2021-10-13 12:36:52 +0200

    Templates and data

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // console.log("Nathan John Giose");
 const app = Vue.createApp({
     //data, funtions, component templates
-    // template: '<h2>I am the template</h2>'
+    data() {
+        return{
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+
+        }
+    }
 })
 
 app.mount('#app')
