98d579ba7d2ef19adb54d00456a5521f12e50679
nathangiose <nathangiose@gmail.com>
2021-10-18 13:34:41 +0200

    toggling fav with click events

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ const app = Vue.createApp({
         handleMousemove(e) {
             this.x = e.offsetX
             this.y = e.offsetY
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
+
         }
         
     }
